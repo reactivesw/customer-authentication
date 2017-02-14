7ef65f66d8571a804d17ec780640ec90f60c7f3a
umasuo <liuquan89@gmail.com>
2017-02-14 10:24:09 +0800

    update build.gradle & enable swagger

--- src/main/java/io/reactivesw/customer/authentication/Application.java
+++ src/main/java/io/reactivesw/customer/authentication/Application.java
@@ -3,14 +3,14 @@ package io.reactivesw.customer.authentication;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by umasuo on 17/2/9.
  */
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
-
-  private static final Logger LOGGER = Logger.getLogger(Application.class);
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
